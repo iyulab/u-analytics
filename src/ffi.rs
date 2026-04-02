ad31c7ec959d82abd26b348079060df05286337d
uj <caveman@iyulab.com>
2026-04-02 13:23:06 +0900

    style: replace unwrap() with expect() in FFI version function

--- src/ffi.rs
+++ src/ffi.rs
@@ -666,5 +666,5 @@ pub unsafe extern "C" fn uanalytics_free_string(ptr: *mut libc::c_char) {
 #[no_mangle]
 pub extern "C" fn uanalytics_version() -> *mut libc::c_char {
     let version = env!("CARGO_PKG_VERSION");
-    CString::new(version).unwrap().into_raw()
+    CString::new(version).expect("version string has no interior NUL").into_raw()
 }
